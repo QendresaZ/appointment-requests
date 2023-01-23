0c7e7b62b88274f96fec5b2e75cfe4366d429d77
QendresaZ <qz40092@ubt-uni.net>
2023-01-23 16:43:59 +0100

    init

---
+++ src/main/java/com/example/appointmentrequestschallange/AppointmentRequestsChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointmentrequestschallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentRequestsChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentRequestsChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appointmentrequestschallange/AppointmentRequestsChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appointmentrequestschallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentRequestsChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
