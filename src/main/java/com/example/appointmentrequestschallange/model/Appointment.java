34d6c307015715f02b90cdcf9a22ad882136bab7
QendresaZ <qz40092@ubt-uni.net>
2023-01-25 09:48:09 +0100

    ADDED Appointment model

--- src/main/java/com/example/appointmentrequestschallange/model/Appointment.java
+++ src/main/java/com/example/appointmentrequestschallange/model/Appointment.java
@@ -1,4 +1,22 @@
 package com.example.appointmentrequestschallange.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Entity
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String service;
+    @Enumerated(EnumType.ORDINAL)
+    private AppointmentState appointmentState;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate appointmentRequestDate;
 }
